dc578bb0108c0cf116ffbabb4a7ca1375546d1ee
Guilherme Mota <guilhermebromonschenkel@gmail.com>
2021-01-15 17:53:18 -0300

    chore(package): add make redirects html file

---
+++ redirectsToHTML.js
@@ -0,0 +1,35 @@
+const fs = require("fs")
+const path = require("path")
+
+const STATIC_FOLDER_NAME = "static"
+
+
+
+const buildRedirectsHTML = (redirects) => {
+	const folderHTML = `
+		<!DOCTYPE html> 
+		<html> 
+			<body>
+				<p>
+					${redirects}
+				</p
+			</body> 
+		</html> 
+	`
+
+	return folderHTML
+}
+
+const redirectsToHTML = async () => {
+	const redirectsFilePath = path.join(__dirname, "_redirects")
+
+	const redirectsFile = await fs.promises.readFile(redirectsFilePath)
+
+	const redirectsHTML = buildRedirectsHTML(redirectsFile)
+
+	const redirectsHTMLPath = path.join(__dirname, "redirects.html")
+
+	await fs.promises.writeFile(redirectsHTMLPath, redirectsHTML)
+}
+
+redirectsToHTML()
