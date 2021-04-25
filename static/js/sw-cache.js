15d35ff20153d626058b7e37bf0f8dc90455bfa5
Guilherme <guilhermebromonschenkel@gmail.com>
2021-04-25 09:47:21 -0300

    chore(*): add all needed commas

--- static/js/sw-cache.js
+++ static/js/sw-cache.js
@@ -5,33 +5,33 @@ window.ServiceWorkerCache = {
 		console.log(`${prefix} ${message}`);
 	},
 	clearAllServiceWorkerCache: async function () {
-		const cacheNames = await caches.keys()
+		const cacheNames = await caches.keys();
 
 		try {
 			Promise.all(
 				cacheNames.map(cache => caches.delete(cache)),
-			)
+			);
 		} catch (error) {
-			this.logger(error)
+			this.logger(error);
 		}
 	},
 	setCache: function (key, version) {
-		return localStorage.setItem(key, version)
+		return localStorage.setItem(key, version);
 	},
 	getCache: function (key) {
-		return localStorage.getItem(key)
+		return localStorage.getItem(key);
 	},
 	refreshCacheIfNeeded: async function (key, version) {
-		const currentCache = this.getCache(key)
+		const currentCache = this.getCache(key);
 
-		const isValidCache = currentCache === version
+		const isValidCache = currentCache === version;
 
 		if (!isValidCache) {
-			this.setCache(key, version)
+			this.setCache(key, version);
 	
-			await this.clearAllServiceWorkerCache()
+			await this.clearAllServiceWorkerCache();
 
-			window.location.reload()
+			window.location.reload();
 		}
 	}
 }
